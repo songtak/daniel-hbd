4a96f7f337e838d5732a639181395df663fbb891
songtak <tak.song@paywatchglobal.com>
2024-02-15 18:23:01 +0900

    쓸모없는 코드 제거

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import _ from "lodash";
+
+import "../src/assets/styles/main.css";
+
+function App() {
+  return (
+    <div className="wrapper">
+      <div>대니얼 생일</div>
+    </div>
+  );
+}
+
+export default App;
