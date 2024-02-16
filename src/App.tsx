85f97bae69281e8a20bff57550a27a4032690d73
songtak <tak.song@paywatchglobal.com>
2024-02-16 13:21:17 +0900

    이미지 파일 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,23 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import _ from "lodash";
-
 import "../src/assets/styles/main.css";
 
 function App() {
   return (
     <div className="wrapper">
-      <div>대니얼 생일</div>
+      <div className="main_wrapper">
+        <div className="animated-title">
+          <div className="track">
+            <div className="content blink">
+              섹/시/가/이/대/니/얼/이/태/원/항/시/대/기
+              섹/시/가/이/대/니/얼/이/태/원/항/시/대/기
+            </div>
+          </div>
+        </div>
+        <div className="images">
+          <img src="../src/assets/images/photo_001.jpeg" />
+        </div>
+      </div>
     </div>
   );
 }
